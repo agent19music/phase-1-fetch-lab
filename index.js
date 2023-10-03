a9b50b9057194dfc26029061b8fa0ecee9ba7d3c
seannyandusi <seanmotanya@gmail.com>
2023-10-03 11:13:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 }
+ fetchBooks();
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +20,53 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function fetch5thbook(){
+  fetch('https://anapioficeandfire.com/api/books/5')
+    .then(res => res.json())
+    .then((data) => {
+      const displayBook = document.createElement('ul')
+      const body = document.querySelector('body')
+      body.appendChild(displayBook);
+      displayBook.textContent ='Fifth Book'
+      for(let item in data){
+        displayBook.innerHTML += `
+        <li>${item}: ${data[item]}</li>`
+      }
+    })
+}
+
+fetch5thbook();
+
+function get1031st(){
+  fetch('https://anapioficeandfire.com/api/characters/1031')
+    .then(res => res.json())
+    .then((data) => {
+      const displayCharacter = document.createElement('ul')
+      const body = document.querySelector('body')
+      body.appendChild(displayCharacter);
+      displayCharacter.textContent = '1031st Guy'
+      for(let item in data){
+        displayCharacter.innerHTML += `<li>${item}: ${data[item]}</li>`
+      }
+    })
+}
+
+get1031st();
+
+function getTotalPages() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then((data) => {
+      let totalPages = 0;
+      const pages = document.createElement('p')
+      const body = document.querySelector('body')
+      body.appendChild(pages);
+      for(let book of data){
+        totalPages += book.numberOfPages;
+      }
+      pages.textContent = `Total number of pages is : ${totalPages}`
+    })
+}
+
+getTotalPages();
